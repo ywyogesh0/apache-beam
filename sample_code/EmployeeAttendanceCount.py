0e19c29de7f84f9b84e53461e5305eb22588cc11
Yogesh Walia <ywalia@sapient.com>
2019-12-24 19:56:56 +0000

    Third Commit

--- sample_code/EmployeeAttendanceCount.py
+++ sample_code/EmployeeAttendanceCount.py
@@ -1,7 +1,7 @@
 import apache_beam as beam
 
 with beam.Pipeline() as attendance_count_pipeline:
-    (
+    input_pcollection = (
             attendance_count_pipeline
 
             | "Read data from dept.txt to create PCollection Object" >>
@@ -9,16 +9,40 @@ with beam.Pipeline() as attendance_count_pipeline:
 
             | "Split row on ',' delimiter" >>
             beam.Map(lambda row: row.split(","))
+    )
+
+    accounts_pcollection = (
+            input_pcollection
 
             | "Keep only 'Accounts' department rows" >>
             beam.Filter(lambda row_arr: row_arr[3] == "Accounts")
 
-            | "Create Tuple (key, value) with key -> (Id, Name) and value -> 1" >>
-            beam.Map(lambda filtered_row_arr: ((filtered_row_arr[0], filtered_row_arr[1]), 1))
+            | "Create 'Accounts' Tuple (key, value) with key -> (Id, Name) and value -> 1" >>
+            beam.Map(lambda filtered_row_arr: (("Accounts", filtered_row_arr[0], filtered_row_arr[1]), 1))
 
-            | "GroupByKey + Combiner (optimization) + Reducer (sum)" >>
+            | "'Accounts' -> GroupByKey + Combiner (optimization) + Reducer (sum)" >>
             beam.CombinePerKey(sum)
+    )
 
-            | "Write each element of PCollection Object" >>
+    hr_pcollection = (
+            input_pcollection
+
+            | "Keep only 'HR' department rows" >>
+            beam.Filter(lambda row_arr: row_arr[3] == "HR")
+
+            | "Create 'HR' Tuple (key, value) with key -> (Id, Name) and value -> 1" >>
+            beam.Map(lambda filtered_row_arr: (("HR", filtered_row_arr[0], filtered_row_arr[1]), 1))
+
+            | "'HR' -> GroupByKey + Combiner (optimization) + Reducer (sum)" >>
+            beam.CombinePerKey(sum)
+    )
+
+    merged_pcollection = (
+            (accounts_pcollection, hr_pcollection)
+
+            | "Merge both PCollection object elements to create single unified PCollection object" >>
+            beam.Flatten()
+
+            | "Write each element of merged PCollection Object" >>
             beam.io.WriteToText("attendance_count_output/count")
     )
